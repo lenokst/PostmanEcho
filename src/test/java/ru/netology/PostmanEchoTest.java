3dfe6cc14aa4094064473423dd5521a4ebd1c68e
e.stashevskaya <lenokstash@gmail.com>
2020-10-14 15:21:08 +0300

    faild project

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -16,7 +16,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("headers", hasKey("host"))
+                .body("headers", hasKey("noHost"))
         ;
     }
 }
