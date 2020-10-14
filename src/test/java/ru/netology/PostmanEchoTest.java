351d9a92e927f7377e19a14b36b1f771cb8c67ba
e.stashevskaya <lenokstash@gmail.com>
2020-10-14 15:17:15 +0300

    faild project

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasKey;
+
+public class PostmanEchoTest {
+    @Test
+    void returnKey() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("headers", hasKey("host"))
+        ;
+    }
+}
